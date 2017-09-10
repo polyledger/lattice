dd4c8445a3e0894c8031bc16eb634f09f844e1e4
Matt Rosendin <matt.rosendin@gmail.com>
2017-09-10 14:42:15 -0700

    0.1.dev6

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 setup(
   name = 'lattice',
-  packages = ['lattice'],
-  version = '0.1.dev3',
+  packages = find_packages(),
+  version = '0.1.dev6',
   description = 'A cryptocurrency market data utility package',
   author = 'Matthew Rosendin',
   author_email = 'matthew@polyledger.com',
   url = 'https://github.com/polyledger/lattice',
   download_url = 'https://github.com/polyledger/lattice/archive/v0.1-alpha.tar.gz',
   keywords = ['cryptocurrency', 'gdax', 'csv'],
+  install_requires=[
+    'requests',
+    'python-dateutil'
+  ],
   classifiers = [],
 )
