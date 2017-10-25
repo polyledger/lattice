dd2a0da278a4283d5356f03b16f81badd0e75565
Matt Rosendin <matt.rosendin@gmail.com>
2017-10-24 17:29:46 -0700

    test skeleton

---
+++ tests/test_allocate.py
@@ -0,0 +1,10 @@
+import unittest
+from lattice.optimize import allocate
+
+class TestAllocate(unittest.TestCase):
+
+    def test_something(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
