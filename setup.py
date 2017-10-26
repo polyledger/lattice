285dc997f92977758ca78d9a4abc14d52985c504
Matt Rosendin <matt.rosendin@gmail.com>
2017-10-25 22:20:32 -0700

    0.3.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
   name = 'lattice',
   packages = find_packages(),
-  version = '0.2.dev2',
+  version = '0.3.0',
   description = 'A cryptocurrency market data utility package',
-  author = 'Matthew Rosendin',
-  author_email = 'matthew@polyledger.com',
+  author = 'Matthew Rosendin, Farshad Miraftab',
+  author_email = 'matthew@polyledger.com, farshad@polyledger.com',
   url = 'https://github.com/polyledger/lattice',
   download_url = 'https://github.com/polyledger/lattice/archive/v0.1-alpha.tar.gz',
   keywords = ['cryptocurrency', 'gdax', 'csv'],
