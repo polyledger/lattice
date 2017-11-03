b2746421ab5542e3a13d40da8abeabbf0add1a2f
Matt Rosendin <matt.rosendin@gmail.com>
2017-11-02 21:07:05 -0700

    fix spacing and add package_data keyword

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'lattice',
-  packages = find_packages(),
-  version = '0.3.8',
-  description = 'A cryptocurrency market data utility package',
-  author = 'Matthew Rosendin, Farshad Miraftab',
-  author_email = 'matthew@polyledger.com, farshad@polyledger.com',
-  url = 'https://github.com/polyledger/lattice',
-  download_url = 'https://github.com/polyledger/lattice/archive/v0.1-alpha.tar.gz',
-  keywords = ['cryptocurrency', 'gdax', 'csv'],
-  install_requires=[
-    'requests',
-    'python-dateutil',
-    'matplotlib',
-    'pandas',
-    'scipy',
-    'numpy'
-  ],
-  classifiers = [],
+    name = 'lattice',
+    packages = find_packages(),
+    package_data = {
+        'lattice': ['datasets/*.csv'],
+    },
+    include_package_data=True,
+    version = '0.3.8',
+    description = 'A cryptocurrency market data utility package',
+    author = 'Matthew Rosendin, Farshad Miraftab',
+    author_email = 'matthew@polyledger.com, farshad@polyledger.com',
+    url = 'https://github.com/polyledger/lattice',
+    download_url = 'https://github.com/polyledger/lattice/archive/v0.1-alpha.tar.gz',
+    keywords = ['cryptocurrency', 'gdax', 'csv'],
+    install_requires=[
+        'requests',
+        'python-dateutil',
+        'matplotlib',
+        'pandas',
+        'scipy',
+        'numpy'
+    ],
+    classifiers = [],
 )
