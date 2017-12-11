df3b1d0204c4ec38988570b4652f4b54404c0d4d
Matt Rosendin <matt.rosendin@gmail.com>
2017-12-11 10:28:06 -0800

    0.4.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         'lattice': ['datasets/*.csv'],
     },
     include_package_data=True,
-    version = '0.4.5',
+    version = '0.4.6',
     description = 'A cryptocurrency market data utility package',
     author = 'Matthew Rosendin, Farshad Miraftab',
     author_email = 'matthew@polyledger.com, farshad@polyledger.com',
